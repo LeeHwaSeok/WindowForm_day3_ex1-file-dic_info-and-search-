8f6ccdc118d2393df4d438fb4a6f0d74955b0ad3
20171062@student.anu.ac.kr <20171062@student.anu.ac.kr>
2022-05-16 11:19:57 +0900

    초기 커밋.

---
+++ WindowForm_day3/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace WindowForm_day3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_dic = new System.Windows.Forms.ListBox();
+            this.lb_file = new System.Windows.Forms.ListBox();
+            this.lb_dicinfo = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lb_dic
+            // 
+            this.lb_dic.FormattingEnabled = true;
+            this.lb_dic.ItemHeight = 18;
+            this.lb_dic.Location = new System.Drawing.Point(55, 37);
+            this.lb_dic.Name = "lb_dic";
+            this.lb_dic.Size = new System.Drawing.Size(488, 94);
+            this.lb_dic.TabIndex = 0;
+            // 
+            // lb_file
+            // 
+            this.lb_file.FormattingEnabled = true;
+            this.lb_file.ItemHeight = 18;
+            this.lb_file.Location = new System.Drawing.Point(55, 160);
+            this.lb_file.Name = "lb_file";
+            this.lb_file.Size = new System.Drawing.Size(488, 94);
+            this.lb_file.TabIndex = 1;
+            // 
+            // lb_dicinfo
+            // 
+            this.lb_dicinfo.FormattingEnabled = true;
+            this.lb_dicinfo.ItemHeight = 18;
+            this.lb_dicinfo.Location = new System.Drawing.Point(55, 287);
+            this.lb_dicinfo.Name = "lb_dicinfo";
+            this.lb_dicinfo.Size = new System.Drawing.Size(488, 94);
+            this.lb_dicinfo.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(567, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 73);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(567, 160);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 73);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(567, 287);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(177, 73);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lb_dicinfo);
+            this.Controls.Add(this.lb_file);
+            this.Controls.Add(this.lb_dic);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lb_dic;
+        private System.Windows.Forms.ListBox lb_file;
+        private System.Windows.Forms.ListBox lb_dicinfo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowForm_day3/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//디렉토리 다루기
+using System.IO;
+
+namespace WindowForm_day3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lb_dic.Items.Clear();
+
+            //디렉토리 string형태로 목록찾기 [기본형식]
+            string[] apaths = Directory.GetDirectories(@"C:\WINDOWS\system32");
+            //Environment.SystemDirectory == @"C:\WINDOWS\system32" 
+
+            //foreach 형식으로 각 폴더명을 ib_dic에 추가한다. [기본형식]
+            foreach (string apath in apaths) { lb_dic.Items.Add(apath); }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lb_file.Items.Clear();
+
+            //파일을 string형태로 파일찾기 [기본형식]  -> "*.*" or "*.exe"
+            string[] afiles = Directory.GetFiles(Environment.SystemDirectory, "*.*");
+
+            //foreach 형식으로 각 파일명을 ib_file에 추가한다. [기본형식]
+            foreach (string afile in afiles) { lb_file.Items.Add(afile); }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            lb_dicinfo.Items.Clear();
+
+            //dirinfo는 인스턴스 형식이라 new로 할당해줘야함.
+            //file, dir은 정적형식이라 할당이 필요없음
+            DirectoryInfo dirinfo = new DirectoryInfo(@"C:\Windows");
+
+            lb_dicinfo.Items.Clear();
+
+            //예외처리 생성정보 확인 => Exists
+            if (dirinfo.Exists)
+            { //디렉토리 대략적인 정보
+                lb_dicinfo.Items.Add("전체경로 : " + dirinfo.FullName);
+                lb_dicinfo.Items.Add("디렉토리 이름 : " + dirinfo.Name);
+                lb_dicinfo.Items.Add("생성일 : " + dirinfo.CreationTime);
+                lb_dicinfo.Items.Add("속성 : " + dirinfo.Attributes);
+                lb_dicinfo.Items.Add("루트 : " + dirinfo.Root);
+            }
+
+
+        }
+    }
+}
